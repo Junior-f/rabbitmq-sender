0f9503facd7f34ac926c8d6a79992ff15f5b1e3c
Junior-f <f.junior7710@gmail.com>
2025-01-10 17:59:56 -0300

    feat: add class IChannel

--- Sender/Service/RabbitMqService.cs
+++ Sender/Service/RabbitMqService.cs
@@ -2,35 +2,48 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Threading.Channels;
 
 namespace Sender.Service
 {
     public class RabbitMqService : IRabbitMqService
     {
-        private readonly string _hostname;
-        private readonly ConnectionFactory _factory;
-
-        public RabbitMqService(string hostname)
+        private readonly string _exchange = "new";
+        private readonly IConnection _connection;
+        private readonly IChannel _channel;
+        public RabbitMqService()
         {
-            _hostname = hostname;
-            _factory = new ConnectionFactory() { HostName = _hostname };
+            var factory = new ConnectionFactory()
+            {
+
+                HostName = "localhost"
+            };
+
+            _connection = factory.CreateConnectionAsync("publisher").Result;
+            _channel = _connection.CreateChannelAsync().Result;
+
         }
-        public async Task SendMessage(string queueName, object mensagem)
+        public async Task SendMessage(string queueName, string _routingKey, object mensagem)
         {
             try
             {
-                using var connection = await _factory.CreateConnectionAsync();
-                using var channel = await connection.CreateChannelAsync();
-                await channel.QueueDeclareAsync(queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                await _channel.ExchangeDeclareAsync(exchange: _exchange, type: ExchangeType.Direct, durable: true);
 
+                await _channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                await _channel.QueueBindAsync(queue: queueName, exchange: _exchange, routingKey: _routingKey);
+
+                var type = mensagem.GetType();
 
                 var message = JsonConvert.SerializeObject(mensagem);
                 var body = Encoding.UTF8.GetBytes(message);
 
-                await channel.BasicPublishAsync(exchange: "topic", routingKey: queueName, body: body);
+                Console.WriteLine($"{type.Name} Published ");
 
-                Console.WriteLine($"[RabbitMQ] Mensagem enviada para a fila '{queueName}': {message}");
-                await Task.CompletedTask;
+                await _channel.BasicPublishAsync(_exchange, _routingKey, body);
+
+                Console.WriteLine($"[RabbitMQ] Mensagem enviada para a fila '{_routingKey}': {message}");
             }
             catch (Exception ex)
             {
